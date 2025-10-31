d764cf877349e5265f6a0b50acca859100be17e8
suhas152 <vedasuhaskulkurni@gmail.com>
2025-10-31 23:39:21 +0530

    v1.0

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -1,7 +1,6 @@
 package com.klef;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class BackendSpringbootappApplication {
 
